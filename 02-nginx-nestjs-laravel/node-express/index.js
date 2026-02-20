df7ba53458da1413a1619ff35e9d4f770663f778
Bondarenko Andrey <andrey53bondarenko@gmail.com>
2026-02-20 10:41:28 +0200

    Лабораторна робота №1. Налаштування вебсерверів

---
+++ 02-nginx-nestjs-laravel/node-express/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Express is running!'));
+app.listen(3000, '0.0.0.0', () => console.log('Listening on port 3000'));
